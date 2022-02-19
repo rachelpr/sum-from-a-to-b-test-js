4a92c33097ac73e99abe68dddc53278d18b3f42f
Rachel PR <rplourder@gmail.com>
2022-02-19 09:17:17 -0400

    A function to sum from a to b with recursion

--- index.js
+++ index.js
@@ -1,6 +1,11 @@
-
+//A function to sum all the values from fromN up to toN - with recursion
 function sum(fromN, toN) {
-  // Sum all the values from fromN up to toN
-}
+  if (fromN === toN) {
+    return toN
+  }
+  return fromN + sum(fromN + 1, toN)
+};
+
+console.log(sum(3, 7))
 
 module.exports = sum;
